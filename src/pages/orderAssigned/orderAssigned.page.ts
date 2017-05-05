83b0ef066a35c0e2df960d2997274680ebfe3a11
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-05 20:49:24 +0100

    Cambio en el formato de captura de hora

--- src/pages/orderAssigned/orderAssigned.page.ts
+++ src/pages/orderAssigned/orderAssigned.page.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { Geolocation } from "@ionic-native/geolocation";
 
+import moment from "moment";
+
 import { AngularFire, FirebaseListObservable } from "angularfire2";
 
 @Component({
@@ -15,7 +17,6 @@ export class OrderAssignedPage {
     delivererData: any;
     private lat: any;
     private long: any;
-    private horaCaptura: any;
 
     constructor(private nav: NavController,
                 private navParams: NavParams,
@@ -29,9 +30,8 @@ export class OrderAssignedPage {
         var pos = geolocation.watchPosition({ maximumAge: 10000, enableHighAccuracy: true }).subscribe((data) => {
             this.lat = data.coords.latitude;
             this.long = data.coords.longitude;
-            this.horaCaptura = data.timestamp;
 
-            this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long, horaCapturaGPS: this.horaCaptura});
+            this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long, horaCapturaGPS: moment().format()});
 
         });
 

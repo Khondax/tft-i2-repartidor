a7b53088ac92f5bd52216a86826e30695863be98
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-05 20:19:58 +0100

    Mayor precisión en el GPS, intervalo de 10 segundos, añadir timestamp

--- src/pages/orderAssigned/orderAssigned.page.ts
+++ src/pages/orderAssigned/orderAssigned.page.ts
@@ -26,12 +26,12 @@ export class OrderAssignedPage {
 
         this.delivererData = this.angularFire.database.list('/repartidores')
 
-        var pos = geolocation.watchPosition().subscribe((data) => {
+        var pos = geolocation.watchPosition({ maximumAge: 10000, enableHighAccuracy: true }).subscribe((data) => {
             this.lat = data.coords.latitude;
             this.long = data.coords.longitude;
             this.horaCaptura = data.timestamp;
 
-            this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long});
+            this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long, horaCapturaGPS: this.horaCaptura});
 
         });
 

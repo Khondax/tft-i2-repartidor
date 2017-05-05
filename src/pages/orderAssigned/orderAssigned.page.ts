2311a75faef61fd0ec654d98821fe54332f97e03
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-05 20:09:55 +0100

    Geolocalización activada

--- src/pages/orderAssigned/orderAssigned.page.ts
+++ src/pages/orderAssigned/orderAssigned.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Geolocation } from "@ionic-native/geolocation";
+
+import { AngularFire, FirebaseListObservable } from "angularfire2";
 
 @Component({
     templateUrl: 'orderAssigned.page.html'
 })
 export class OrderAssignedPage {
-    
-    constructor(private nav: NavController) {
+        
+    deliverer: any;
+
+    delivererData: any;
+    private lat: any;
+    private long: any;
+    private horaCaptura: any;
+
+    constructor(private nav: NavController,
+                private navParams: NavParams,
+                private geolocation: Geolocation,
+                private angularFire: AngularFire) {
+
+        this.deliverer = navParams.data;
+
+        this.delivererData = this.angularFire.database.list('/repartidores')
+
+        var pos = geolocation.watchPosition().subscribe((data) => {
+            this.lat = data.coords.latitude;
+            this.long = data.coords.longitude;
+            this.horaCaptura = data.timestamp;
+
+            this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long});
+
+        });
 
     }
 

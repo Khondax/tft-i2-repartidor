8bc9bc40a4c25a0dd80adbc60238bf381d79d0f7
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-05 13:28:43 +0100

    Arreglo clase

--- src/pages/delivery/delivery.page.ts
+++ src/pages/delivery/delivery.page.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'delivery.page.html'
+    templateUrl: 'delivery.page.html'
 })
-export class Delivery {
 
-  constructor(private navCtrl: NavController) {
+export class DeliveryPage {
 
-  }
+    constructor(private nav: NavController) {
+
+    }
 
 }

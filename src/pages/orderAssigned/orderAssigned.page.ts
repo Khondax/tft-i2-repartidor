d120b05cc19dc2d27c516d3ec911887db8b342c8
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-09 12:19:03 +0100

    Se muestran pedidos (requiere cambios)

--- src/pages/orderAssigned/orderAssigned.page.ts
+++ src/pages/orderAssigned/orderAssigned.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 
+import _ from 'lodash';
+import { AngularFire, FirebaseListObservable } from "angularfire2";
+import moment from "moment";
 import { Geolocation } from "@ionic-native/geolocation";
 
-import moment from "moment";
-
-import { AngularFire, FirebaseListObservable } from "angularfire2";
+import { ScanPage, MapPage } from "../pages";
 
 @Component({
     templateUrl: 'orderAssigned.page.html'
@@ -19,8 +20,14 @@ export class OrderAssignedPage {
     private lat: any;
     private long: any;
 
+    private ordersData: any;
+    assignedOrders = [];
+    assignedOrdersData: any;
+
+
     constructor(private nav: NavController,
                 private navParams: NavParams,
+                private loadingController: LoadingController,
                 private geolocation: Geolocation,
                 private angularFire: AngularFire) {
 
@@ -39,6 +46,40 @@ export class OrderAssignedPage {
             this.delivererData.update(this.deliverer.$key, {latitud: this.lat, longitud: this.long, horaCapturaGPS: moment().format()});
 
         });
+
+        let loader = this.loadingController.create({
+            content: 'Obteniendo datos...',
+            spinner: 'bubbles'
+        });
+
+        loader.present().then(() => {
+            //TODO: Hay que mostrarlos por repartidor
+            this.angularFire.database.list('/pedidos').subscribe(data => {
+                this.ordersData = _.chain(data)
+                                  .filter(o => o.estado === "En el almacén")
+                                  .groupBy('codigoPostal')
+                                  .toPairs()
+                                  .map(item => _.zipObject(['codPos', 'pedido'], item))
+                                  .value();
+
+                this.assignedOrdersData = _.chain(this.ordersData)
+                                          .orderBy('direccion')
+                                          .value();
+
+                this.assignedOrders = this.assignedOrdersData;
+
+                loader.dismiss();
+            });
+        });
+
+    }
+
+    goToScan($event, order){
+        this.nav.push(ScanPage, order);
+    }
+
+    goToMap($event, order){
+        this.nav.push(MapPage, order);
     }
 
 

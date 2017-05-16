c36684a1e5c9e299fc50781f08b7e1f3ffdc0767
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-05-16 15:28:40 +0100

    Métodos para mostrar y controlar el panel de firma

--- src/pages/drawpad/drawpad.page.ts
+++ src/pages/drawpad/drawpad.page.ts
@@ -1,29 +1,57 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 
-import { AngularFire } from "angularfire2";
+import { SignaturePad } from "angular2-signaturepad/signature-pad";
 
+import { ScanPage } from "../pages";
 
 @Component({
-    templateUrl: 'drawpad.page.html'
+    templateUrl: 'drawpad.page.html',
+    selector: 'drawpad.page.scss'
 })
 
 export class DrawpadPage {
 
-    constructor(private nav: NavController,
-                private navParams: NavParams,
-                private loadingController: LoadingController) {
+    @ViewChild(SignaturePad) private signaturePad: SignaturePad;
+    
+    public signaturePadOptions: Object = {
+        'minWidth': 2,
+        'canvasWidth': 340,
+        'canvasHeight': 400
+    };
 
+    private signatureImage: string;
+    private order: any;
 
+    constructor(private nav: NavController, private navParams: NavParams) {
+
+        this.order = navParams.data;
     }
 
-    ionViewDidLoad(){
-        
+    canvasResize() {
+        let canvas = document.querySelector('canvas');
+        this.signaturePad.set('minWidth', 1);
+        this.signaturePad.set('canvasWidth', canvas.offsetWidth);
+        this.signaturePad.set('canvasHeight', canvas.offsetHeight);
     }
 
-    save(){
-
+    ngAfterViewInit() {
+        this.signaturePad.clear();
+        this.canvasResize();
     }
 
+    drawCancel(){
+        this.nav.pop();
+    }
+
+
+    drawClear(){
+        this.signaturePad.clear();
+    }
+
+    drawComplete(){
+        this.signatureImage = this.signaturePad.toDataURL();
+        this.nav.push (ScanPage, {signatureImage: this.signatureImage, data: this.order});
+    }
 
 }

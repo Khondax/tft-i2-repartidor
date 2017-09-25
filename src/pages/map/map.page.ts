052cdb340f0c089c1db0c36cfe2afcd7a0a8088f
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-09-25 18:25:25 +0100

    Mapa capaz de mostrar ruta óptima de entrega

--- src/pages/map/map.page.ts
+++ src/pages/map/map.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 
+declare var window: any;
 
 @Component({
     templateUrl: 'map.page.html'
 })
 export class MapPage {
 
+    map: any = {};
 
     constructor(private nav: NavController,
                 private navParams: NavParams,
@@ -15,6 +17,19 @@ export class MapPage {
 
     }
 
+    ionViewDidLoad(){
+        let order = this.navParams.data;
 
+        this.map = {
+            lat: order.latitud,
+            lng: order.longitud,
+            zoom: 16,
+            markerLabel: order.direccion
+        };
+    }
+
+    getDirection(){
+        window.location = `geo:${this.map.lat},${this.map.lng};u=35`;
+    }
 
 }

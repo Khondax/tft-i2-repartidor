08f64acf926658aa207e6fda8c137b00ea4ae447
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-10-19 11:49:30 +0100

    Mensaje para añadir o no una incidencia en la entrega

--- src/pages/scan/scan.page.ts
+++ src/pages/scan/scan.page.ts
@@ -80,13 +80,35 @@ import { DrawpadPage, HomePage } from "../pages";
             }, 300);
 
         }
-
-
     }
 
     finishDeliver(){
 
-        this.orderData.update(this.order.$key, {fechaEntrega: moment().format(), estado: "Entregado", firma: this.signatureImage});
+        let prompt = this.alertController.create({
+            title: 'Nueva incidencia',
+            message: "¿Desea añadir una incidencia?",
+            inputs: [{
+                type: 'text',
+                name: 'incidencia'
+            }],
+            buttons: [
+                {
+                    text: 'Descartar',
+                    handler: data =>{
+                        this.orderData.update(this.order.$key, {fechaEntrega: moment().format(), estado: "Entregado", firma: this.signatureImage});
+                    }
+                },
+                {
+                    text: 'Añadir',
+                    handler: data => {
+                        this.orderData.update(this.order.$key, {fechaEntrega: moment().format(), estado: "Entregado con incidencia", firma: this.signatureImage, observaciones: data.incidencia});
+
+                    }
+                }
+            ]
+        });
+
+        prompt.present();        
 
         let toast = this.toastController.create({
             message: "Pedido entregado",
